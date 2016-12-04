c644d08707e658c08cdb96605ae4c1a46b1f3e0e
LanceMaverick <pnoel6@gmail.com>
2016-12-04 22:38:09 +0100

    PyndicoEventResponse class implemented

--- pyndico.py
+++ pyndico.py
@@ -1,9 +1,61 @@
-import os
 import hashlib
 import hmac
 import urllib
 import time
 import json
+
+class PyndicoEventResponse:
+    def __init__(self, result_json):
+        self.response = result_json
+        self._set_attributes()
+    
+    def _set_attributes(self):
+        self.room = self.response['roomFullname']
+        self.id = self.response['id']
+        self.category = self.response['category']
+        self.start = self.response['startDate']
+        self.start_date = self.start['date']
+        self.start_time = self.start['time']
+        self.time_zone = self.start['tz']
+        self.location = self.response['location']
+        self.title = self.response['title']
+        self.contributions = self.response['contributions']
+        self.catalogue = [
+                self._catalogue_contribution(c) 
+                for c in self.contributions
+                if c['startDate'] != None]
+    
+    def _catalogue_contribution(self, c):
+        start =  c['startDate']
+        time = start['time']
+        tz = start['tz']
+
+        title = c['title']
+        speakers = [n['fullName'] for n in c['speakers']]
+        attachments = []
+        for f in c['folders']:
+            for a in f['attachments']:
+                if a['type'] == 'file':
+                    attachments.append(
+                            dict(
+                                filename = a['filename'],
+                                title = a['title'],
+                                url = a['download_url'],
+                                ))
+        return dict(
+            title = title,
+            speakers = speakers,
+            attachments = attachments,
+            time = time,
+            tz = tz
+            )
+    def __str__(self):
+        return '{}, {}, {}-{}-{}'.format(
+                self.title, 
+                self.id, 
+                self.start_date, 
+                self.start_time, 
+                self.time_zone)
 
 class Pyndico:
     '''Simple python wrapper for the CERN Indico API'''
@@ -36,7 +88,7 @@ class Pyndico:
             if not persistent:
                 items.append(('timestamp', str(int(time.time()))))
             items = sorted(items, key=lambda x: x[0].lower())
-            url = '%s?%s' % (self.base_url+path, urllib.urlencode(items))
+            url = '%s?%s' % (path, urllib.urlencode(items))
             signature = hmac.new(self.secret_key, url, hashlib.sha1).hexdigest()
             items.append(('signature', signature))
         if not items:
@@ -62,22 +114,30 @@ class Pyndico:
         request_url = self.build_request(
                 ''.join(path),
                 params)
-        print(request_url)
         response = urllib.urlopen(request_url)
-        print(response)
 
         return json.load(response)
 
     def get_event(self, **params):
         params.update(dict(qtype = 'event'))
-        return self.get(**params)
+        response =  self.get(**params)
+        event_results = []
+        for result in response['results']:
+            try:
+                event_results.append(
+                        PyndicoEventResponse(result))
+            except KeyError as e:
+                print('Malformed Response: ', e, result)
+        return event_results       
     
     def get_category(self, **params):
         params.update(dict(qtype = 'categ'))
+        #TODO category response object
         return self.get(**params)
     
     def get_room(self, **params):
         params.update(dict(qtype = 'room'))
+        #TODO room response object
         return self.get(**params)
 
 
